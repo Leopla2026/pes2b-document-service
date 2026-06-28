d90dfa7fdb7f2ca14fecf00dd44e0c0917165fc7
Leopla2026 <leonardo@jorgepla.com.br>
2026-06-28 15:35:00 -0300

    Fix PGDAS declaration parser index

--- src/document/parsers/simples/declaracao/index.js
+++ src/document/parsers/simples/declaracao/index.js
@@ -1,23 +1,16 @@
 const parseIdentificacao = require('./parse.identificacao');
 const parseResumo = require('./parse.resumo');
 const parseReceitas = require('./parse.receitas');
-recepcao: parseRecepcao(text),
-
+const parseRecepcao = require('./parse.recepcao');
 
 module.exports.parse = (text) => {
 
     return {
-
         identificacao: parseIdentificacao(text),
-
         receitas: parseReceitas(text),
-
         resumo: parseResumo(text),
-
-	recepcao: parseRecepcao(text),
-
+        recepcao: parseRecepcao(text),
         extras: {}
-
     };
 
 };
